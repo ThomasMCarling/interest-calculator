233a1aa1494bab1ac249d9f602d3e8a7304248fd
Thomas Carling <thomasmcarling@gmail.com>
2025-06-20 19:19:52 +0100

    add first input

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,48 +1,49 @@
 // src/routes/index.tsx
-import * as fs from "node:fs";
-import { createFileRoute, useRouter } from "@tanstack/react-router";
-import { createServerFn } from "@tanstack/react-start";
-
-const filePath = "count.txt";
-
-async function readCount() {
-  return parseInt(
-    await fs.promises.readFile(filePath, "utf-8").catch(() => "0")
-  );
-}
-
-const getCount = createServerFn({
-  method: "GET",
-}).handler(() => {
-  return readCount();
-});
-
-const updateCount = createServerFn({ method: "POST" })
-  .validator((d: number) => d)
-  .handler(async ({ data }) => {
-    const count = await readCount();
-    await fs.promises.writeFile(filePath, `${count + data}`);
-  });
+import { createFileRoute } from "@tanstack/react-router";
+import { cn } from "../cn";
 
 export const Route = createFileRoute("/")({
   component: Home,
-  loader: async () => await getCount(),
+  validateSearch: (search) => ({
+    ...(typeof search.bank === "string" && search.bank !== ""
+      ? { bank: search.bank }
+      : {}),
+  }),
 });
 
 function Home() {
-  const router = useRouter();
-  const state = Route.useLoaderData();
+  const search = Route.useSearch();
+  const navigate = Route.useNavigate();
 
   return (
-    <button
-      type="button"
-      onClick={() => {
-        void updateCount({ data: 1 }).then(() => {
-          void router.invalidate();
-        });
-      }}
-    >
-      Add 1 to {state}?
-    </button>
+    <div className="grid grid-cols-2 gap-2 text-right pt-10 mx-auto max-w-md">
+      <label htmlFor="bank">Bank</label>
+      <Input
+        key="bank"
+        id="bank"
+        value={search.bank ?? ""}
+        type="text"
+        onChange={(e) => {
+          void navigate({
+            to: "/",
+            search: { ...(e.target.value ? { bank: e.target.value } : {}) },
+            replace: true,
+            resetScroll: false,
+          });
+        }}
+      />
+    </div>
+  );
+}
+
+function Input({ className, ...props }: React.ComponentProps<"input">) {
+  return (
+    <input
+      className={cn(
+        "border-2 pl-1 rounded-md focus-within:outline-1 outline-gray-300 ",
+        className
+      )}
+      {...props}
+    />
   );
 }
