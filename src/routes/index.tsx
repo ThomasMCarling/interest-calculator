34f97096b0d6b7fc97ed002a9626340e32c35c28
Thomas Carling <thomasmcarling@gmail.com>
2025-06-20 20:43:35 +0100

    debounce search param updates

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,8 @@
 // src/routes/index.tsx
 import { createFileRoute } from "@tanstack/react-router";
 import { cn } from "../cn";
+import { useRef } from "react";
+import { useDebouncedCallback } from "use-debounce";
 
 export const Route = createFileRoute("/")({
   component: Home,
@@ -8,29 +10,49 @@ export const Route = createFileRoute("/")({
     ...(typeof search.bank === "string" && search.bank !== ""
       ? { bank: search.bank }
       : {}),
+    ...(typeof search.accountName === "string" && search.accountName !== ""
+      ? { accountName: search.accountName }
+      : {}),
   }),
 });
 
 function Home() {
   const search = Route.useSearch();
   const navigate = Route.useNavigate();
+  const formValues = useRef(search);
+  const changeSearchParams = useDebouncedCallback((arg: typeof search) => {
+    void navigate({
+      to: "/",
+      search: arg,
+      replace: true,
+      resetScroll: false,
+    });
+  }, 500);
+
+  const handleFieldChange = (
+    field: keyof typeof search
+  ): React.ChangeEventHandler<HTMLInputElement> => {
+    return (e) => {
+      formValues.current[field] = e.target.value || undefined;
+      void changeSearchParams(formValues.current);
+    };
+  };
 
   return (
     <div className="grid grid-cols-2 gap-2 text-right pt-10 mx-auto max-w-md">
       <label htmlFor="bank">Bank</label>
       <Input
-        key="bank"
         id="bank"
-        value={search.bank ?? ""}
+        defaultValue={search.bank}
         type="text"
-        onChange={(e) => {
-          void navigate({
-            to: "/",
-            search: { ...(e.target.value ? { bank: e.target.value } : {}) },
-            replace: true,
-            resetScroll: false,
-          });
-        }}
+        onChange={handleFieldChange("bank")}
+      />
+      <label htmlFor="accountName">Account Name</label>
+      <Input
+        type="text"
+        id="accountName"
+        defaultValue={search.accountName}
+        onChange={handleFieldChange("accountName")}
       />
     </div>
   );
